828b25a33067a6b8a0ed7e58fcf00bcb181a742d
Andreas Marek <andimarek@fastmail.fm>
2020-05-26 17:04:05 +1000

    change header color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: '#e535ab',
       marginBottom: '1.45rem',
     }}
   >
